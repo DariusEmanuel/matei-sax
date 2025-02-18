52f8c11718d062bc9fbedbcc33705add6776dd52
opread <opread@cngroup.dk>
2025-02-18 23:01:52 +0200

    try to fix parallex on ios

--- src/components/LandingHero.vue
+++ src/components/LandingHero.vue
@@ -17,9 +17,10 @@
   align-items: center;
   justify-content: center;
   background-image: url("../assets/hero/hero.jpg");
-  background-size: cover;
+  /* background-size: cover; */
   /* Scale the background to be 110% of the container's height */
   /* background-size: auto 110%; */
+  background-size: auto 110%;
   background-position: top center;
   background-repeat: no-repeat;
   background-attachment: fixed;
